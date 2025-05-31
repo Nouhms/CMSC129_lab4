e23e807fc5136ce20db17485c51ed652a6f86349
Andry T <andryjosephtumacole@gmail.com>
2025-05-31 21:17:04 +0800

    [FIX][AJ] moved keys to env

--- src/firebase/app.ts
+++ src/firebase/app.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNWqmb6oNiBGWOYmQJvQmW4Rw982VMh9c",
-  authDomain: "todo-e1e59.firebaseapp.com",
-  projectId: "todo-e1e59",
-  storageBucket: "todo-e1e59.firebasestorage.app",
-  messagingSenderId: "10406630140",
-  appId: "1:10406630140:web:6638dc8953584980bd3d79",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
